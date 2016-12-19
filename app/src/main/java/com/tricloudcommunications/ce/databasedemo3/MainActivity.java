0747219745faade78209650240bf80be7f9d3b2c
“Ori <chandleretienne@gmail.com>
2016-12-18 22:32:52 -0600

    update

--- app/src/main/java/com/tricloudcommunications/ce/databasedemo3/MainActivity.java
+++ app/src/main/java/com/tricloudcommunications/ce/databasedemo3/MainActivity.java
@@ -23,32 +23,69 @@ public class MainActivity extends AppCompatActivity {
 
 
         try {
-            SQLiteDatabase usersDB = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
-            //usersDB.execSQL("CREATE TABLE IF NOT EXISTS users (userName VARCHAR, userAge INT(3))");
-            //usersDB.execSQL("INSERT INTO users (userName, userAge) VALUES('Chandler', 37)");
-            //usersDB.execSQL("INSERT INTO users (userName, userAge) VALUES('Triphose', 35)");
-            //usersDB.execSQL("INSERT INTO users (userName, userAge) VALUES('Milan', 10)");
-            //usersDB.execSQL("INSERT INTO users (userName, userAge) VALUES('Laila', 8)");
+            SQLiteDatabase usersDB = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);//Opens or creates the database
 
+            /** CREATE table if not exists
+             *
+             * usersDB.execSQL("CREATE TABLE IF NOT EXISTS users (userName VARCHAR, userAge INT(3))");
+             * usersDB.execSQL("CREATE TABLE IF NOT EXISTS newUsers (userName VARCHAR, userAge INT(3), id INTEGER PRIMARY KEY)");
+             *
+             * */
+
+             /** INSERT DATA
+             * *
+              usersDB.execSQL("INSERT INTO users (userName, userAge) VALUES('Chandler', 37)");
+              usersDB.execSQL("INSERT INTO users (userName, userAge) VALUES('Triphose', 35)");
+              usersDB.execSQL("INSERT INTO users (userName, userAge) VALUES('Milan', 10)");
+              usersDB.execSQL("INSERT INTO users (userName, userAge) VALUES('Laila', 8)");
+              usersDB.execSQL("INSERT INTO users (userName, userAge) VALUES('Chandler', 21)");
+
+              usersDB.execSQL("INSERT INTO newUsers (userName, userAge) VALUES('Chandler', 37)");
+              usersDB.execSQL("INSERT INTO newUsers (userName, userAge) VALUES('Triphose', 35)");
+              usersDB.execSQL("INSERT INTO newUsers (userName, userAge) VALUES('Milan', 10)");
+              usersDB.execSQL("INSERT INTO newUsers (userName, userAge) VALUES('Laila', 8)");
+              usersDB.execSQL("INSERT INTO newUsers (userName, userAge) VALUES('Chandler', 21)");
+             * */
+
+            /** DELETE DATA
+             *
+             * //usersDB.execSQL("DELETE FROM users WHERE userName='Chandler' AND userAge=21 LIMIT 1");
+             *
+             * */
+
+
+            /** UPDATE DATA
+             *
+             * //usersDB.execSQL("UPDATE users SET userAge = 25 WHERE userName ='Laila' ");
+             *
+             * */
+
+            /** Query the data in Database
+            Cursor c = usersDB.rawQuery("SELECT * FROM newUsers", null); //Gets all record in that users Table
             //Cursor c = usersDB.rawQuery("SELECT * FROM users", null); //Gets all record in that users Table
             //Cursor c = usersDB.rawQuery("SELECT * FROM users WHERE userAge < 18", null);//Searched for all records where the userAge field has a value of less than 18
             //Cursor c = usersDB.rawQuery("SELECT * FROM users WHERE userName = 'Milan' AND userAge < 20", null);//Searched for all records where the userName='Milan' and the userAge<20
             //Cursor c = usersDB.rawQuery("SELECT * FROM users WHERE userName = 'Milan' AND userAge = 20", null);//Searches for all records where the userName='Milan' and the userAge = 20
             //Cursor c = usersDB.rawQuery("SELECT * FROM users WHERE userName LIKE 'L%'", null);//Searched for all records where the userName is LIKE it starts with a capital letter 'L' 'L%'
             //Cursor c = usersDB.rawQuery("SELECT * FROM users WHERE userName LIKE '%n'", null);//Searched for all records where the userName is LIKE it ends with a lower case letter 'n' '%n'
-            Cursor c = usersDB.rawQuery("SELECT * FROM users WHERE userName LIKE '%o%'", null);//Searched for all records where the userName is LIKE it has a lower case letter 'o' inside of the string '%o%'
+            //Cursor c = usersDB.rawQuery("SELECT * FROM users WHERE userName LIKE '%o%'", null);//Searched for all records where the userName is LIKE it has a lower case letter 'o' inside of the string '%o%'
+            //Cursor c = usersDB.rawQuery("SELECT * FROM users WHERE userName LIKE '%a%' LIMIT 1", null);//Searched for all records 'WHERE' the userName is 'LIKE' it has a lower case letter 'a' inside of the string '%o%' and 'LIMIT' result to one record
 
             int userNameIndex = c.getColumnIndex("userName");
             int userAgeIndex = c.getColumnIndex("userAge");
+            int userIDIndex = c.getColumnIndex("id");
 
             c.moveToFirst();
 
             while (c != null) {
 
-                Log.i("Database Results", "User Name: " + c.getString(userNameIndex) + " User Age: " + c.getString(userAgeIndex));
+                Log.i("Database Results", "User Name: " + c.getString(userNameIndex) + " User Age: " + Integer.toString(c.getInt(userAgeIndex)) + " ID: " + Integer.toString(c.getInt(userIDIndex)));
 
                 c.moveToNext();
             }
+            */
+
+
         }catch (Exception e){
 
             e.printStackTrace();
